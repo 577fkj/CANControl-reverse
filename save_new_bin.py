55f3af8342fee4ce6874ce5a53f3c783eaf35740
方块君 <fkj@fkj233.cn>
2024-03-24 21:27:56 +0800

    Update

--- save_new_bin.py
+++ save_new_bin.py
@@ -1,15 +1,26 @@
+from datetime import datetime
+
 import requests
 
 url = 'http://bin.bemfa.com/b/{}3BcOGM0ZDJiN2ZkMGU3NDk0ZWEwMzkwNGU2ZDBmYWNhZDc=CANControl.bin'
 
-for i in range(200):
-    r = requests.get(url.format(i))
+def download_bin(bin_id, next_bin = None):
+    r = requests.get(url.format(bin_id))
     if r.status_code != 200:
-        print(f'Error: {r.status_code}')
-        continue
-    print(f'Writing {i}')
-    with open(f'bin\CANControl_{i}.bin', 'wb') as f:
-        f.write(r.content)
+        print(f'Error({bin_id}): {r.status_code}')
+        return False
+    data = r.content
+    name = bin_id if not next_bin else next_bin
+    name = f'CANControl_{name}.bin'
+    print(f'Saving {name}')
+    with open(f'bin/{name}', 'wb') as f:
+        f.write(data)
+    return True
+
+for i in range(1, 200):
+    if not download_bin(i):
+        download_bin('', i)
+        break
 
 if __name__ == '__main__':
     print('Done')
