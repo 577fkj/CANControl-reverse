68f40c76f0b65ff340acb6c9b80f1071ee69940a
方块君 <fkj@fkj233.cn>
2024-08-10 18:37:00 +0800

    Fix action

--- fw/save_new_bin.py
+++ fw/save_new_bin.py
@@ -40,17 +40,37 @@ def download_bin(bin_id, next_bin = None):
 def download_test_new_ui(bin_id, next_bin = None):
     return download(test_new_ui, 'NewTest', 'bin_test', bin_id, next_bin)
 
-for i in range(1, 200):
+new_files = os.listdir('bin_new')
+test_files = os.listdir('bin_test')
+files = os.listdir('bin')
+
+new_files = [int(f.split('_')[0]) for f in new_files]
+test_files = [int(f.split('_')[0]) for f in test_files]
+files = [int(f.split('_')[0]) for f in files]
+
+new_files.sort()
+test_files.sort()
+files.sort()
+
+new_start = new_files[-1] if new_files else 1
+test_start = test_files[-1] if test_files else 1
+start = files[-1] if files else 1
+
+print(f'New start: {new_start}')
+print(f'Test start: {test_start}')
+print(f'Bin start: {start}')
+
+for i in range(start, 500):
     if not download_bin(i):
         download_bin('', i)
         break
 
-for i in range(1, 200):
+for i in range(new_start, 500):
     if not download_new_ui(i):
         download_new_ui('', i)
         break
 
-for i in range(1, 200):
+for i in range(test_start, 500):
     if not download_test_new_ui(i):
         download_test_new_ui('', i)
         break
