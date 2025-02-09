1484b1541832f4508900bde33bc25600a17506b4
方块君 <fkj@fkj233.cn>
2025-02-09 17:33:51 +0800

    Update

--- fw/save_new_bin.py
+++ fw/save_new_bin.py
@@ -5,6 +5,8 @@ url = 'http://bin.bemfa.com/b/{}3BcOGM0ZDJiN2ZkMGU3NDk0ZWEwMzkwNGU2ZDBmYWNhZDc=C
 new_ui = 'http://bin.bemfa.com/b/{}3BcOGM0ZDJiN2ZkMGU3NDk0ZWEwMzkwNGU2ZDBmYWNhZDc=NewUI.bin'
 test_new_ui = 'http://bin.bemfa.com/b/{}3BcOGM0ZDJiN2ZkMGU3NDk0ZWEwMzkwNGU2ZDBmYWNhZDc=NewTest.bin'
 
+devid = 'https://bin.bemfa.com/b/{}3BcOGM0ZDJiN2ZkMGU3NDk0ZWEwMzkwNGU2ZDBmYWNhZDc=DEVID.bin'
+
 def get_version(data):
     # find version string '00 35 2E ? ? 00'
     version = b'Unk'
@@ -14,7 +16,7 @@ def get_version(data):
             break
     return version.decode('utf-8')
 
-def download(url, file_name, save_dir, bin_id, next_bin = None):
+def download(url, file_name, save_dir, bin_id, next_bin = None, find_version = True):
     r = requests.get(url.format(bin_id))
     if r.status_code != 200:
         print(f'Error({bin_id}): {r.status_code}')
@@ -22,10 +24,13 @@ def download(url, file_name, save_dir, bin_id, next_bin = None):
     data = r.content
     file_id = bin_id if not next_bin else next_bin
     name = f'{file_name}_{file_id}.bin'
-    version = get_version(data)
+    if find_version:
+        version = '_' + get_version(data)
+    else:
+        version = ''
     if os.path.exists(f'{save_dir}/{name}'):
         os.remove(f'{save_dir}/{name}')
-    name = f'{file_id}_{file_name}_{version}.bin'
+    name = f'{file_id}_{file_name}{version}.bin'
     print(url.format(bin_id))
     print(f'Saving {name}')
     with open(f'{save_dir}/{name}', 'wb') as f:
@@ -41,39 +46,52 @@ def download_bin(bin_id, next_bin = None):
 def download_test_new_ui(bin_id, next_bin = None):
     return download(test_new_ui, 'NewTest', 'bin_test', bin_id, next_bin)
 
+def download_devid(bin_id, next_bin = None):
+    return download(devid, 'DEVID', 'devid', bin_id, next_bin, False)
+
 new_files = os.listdir('bin_new')
 test_files = os.listdir('bin_test')
 files = os.listdir('bin')
+devid_files = os.listdir('devid')
 
 new_files = [int(f.split('_')[0]) for f in new_files]
 test_files = [int(f.split('_')[0]) for f in test_files]
 files = [int(f.split('_')[0]) for f in files]
+devid_files = [int(f.split('_')[0]) for f in devid_files]
 
 new_files.sort()
 test_files.sort()
 files.sort()
+devid_files.sort()
 
 new_start = new_files[-1] if new_files else 1
 test_start = test_files[-1] if test_files else 1
 start = files[-1] if files else 1
+devid_start = devid_files[-1] if devid_files else 1
 
 print(f'New start: {new_start}')
 print(f'Test start: {test_start}')
 print(f'Bin start: {start}')
+print(f'Devid start: {devid_start}')
 
-for i in range(start, 500):
+for i in range(start, 999):
     if not download_bin(i):
         download_bin('', i)
         break
 
-for i in range(new_start, 500):
+for i in range(new_start, 999):
     if not download_new_ui(i):
         download_new_ui('', i)
         break
 
-for i in range(test_start, 500):
+for i in range(test_start, 999):
     if not download_test_new_ui(i):
         download_test_new_ui('', i)
+        break
+
+for i in range(devid_start, 999):
+    if not download_devid(i):
+        download_devid('', i)
         break
 
 if __name__ == '__main__':
