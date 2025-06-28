c5c8b7c7b7b1676b15df858f52199fc95e4b094d
方块君 <fkj@fkj233.cn>
2025-06-28 11:26:06 +0800

    Update

--- fw/save_new_bin.py
+++ fw/save_new_bin.py
@@ -2,6 +2,7 @@ import requests
 import os
 import json
 import hashlib
+import random
 
 CANControl_save_path = 'CANControl'
 NewUI_save_path = 'NewUI'
@@ -32,8 +33,56 @@ base_url = 'http://bin.bemfa.com/b/{}3BcOGM0ZDJiN2ZkMGU3NDk0ZWEwMzkwNGU2ZDBmYWNh
 
 test0_url = 'https://6ud6631us995.vicp.fun/firmware.bin'
 
+def get_device_info(firmware: bytes) -> dict:
+    max_flash_size = 4194304  # 4MB
+    free_space = max_flash_size - len(firmware)
+    m5 = md5(firmware)
+    s256 = sha256(firmware).upper()
+
+
+    sta_mac, ap_mac = generate_mac_address()
+
+    return {
+        'User-Agent': 'ESP32-http-Update',
+        'Connection': 'close',
+        'Cache-Control': 'no-cache',
+        'x-ESP32-STA-MAC': sta_mac,
+        'x-ESP32-AP-MAC': ap_mac,
+        'x-ESP32-free-space': str(free_space),
+        'x-ESP32-sketch-size': str(len(firmware)),
+        'x-ESP32-sketch-md5': m5,
+        'x-ESP32-sketch-sha256': s256,
+        'x-ESP32-chip-size': str(max_flash_size),
+        'x-ESP32-sdk-version': 'v4.4.5',
+        'x-ESP32-mode': 'sketch',
+    }
+
+def generate_mac_address():
+    base_mac = [random.randint(0, 255) for _ in range(6)]
+    base_mac[0] = base_mac[0] & 0xFC  # 11111100
+    sta_mac = base_mac[:]
+    ap_mac = base_mac[:]
+    ap_mac[0] = (ap_mac[0] | 0x02) & 0xFE # 00000010
+    carry = 1
+    for i in range(5, -1, -1):
+        if carry == 0:
+            break
+        new_value = ap_mac[i] + carry
+        carry = new_value // 256
+        ap_mac[i] = new_value % 256
+    
+    def format_mac(mac_bytes):
+        return ":".join(f"{byte:02X}" for byte in mac_bytes)
+    
+    return format_mac(sta_mac), format_mac(ap_mac)
+
 def md5(data: bytes) -> str:
     m = hashlib.md5()
+    m.update(data)
+    return m.hexdigest()
+
+def sha256(data: bytes) -> str:
+    m = hashlib.sha256()
     m.update(data)
     return m.hexdigest()
 
@@ -172,10 +221,18 @@ def download_file(result: list, file: str, save_path: str, start: int, max_count
 
             result.append(info)
 
-def download_test0(Test0, save_path: str):
+def download_test0(Test0, save_path: str, last_firmware_path: str):
     print('Downloading Test0 from test0_url')
     try:
-        r = requests.get(test0_url, timeout=30)
+        if os.path.exists(last_firmware_path):
+            with open(last_firmware_path, 'rb') as f:
+                headers = get_device_info(f.read())
+            print('Using last firmware file headers')
+            print(json.dumps(headers, indent=4, ensure_ascii=False))
+        else:
+            print('Last firmware file not found, using default headers')
+            headers = {}
+        r = requests.get(test0_url, timeout=30, headers=headers)
         if r.status_code != 200:
             print(f'Error downloading Test0: {r.status_code}')
             return
@@ -218,13 +275,19 @@ def download_all(max_count: int = 999):
     Test2_start = Test2[-1]['count'] if Test2 else 0
     DEVID_start = DEVID[-1]['count'] if DEVID else 0
 
+    # random get last firmware
+    black_list = [CANControl, DEVID, Test0]
+    last_firmware = random.choice(list(filter(lambda x: x[1] not in black_list , bin_info.items())))
+    last_firmware_path = os.path.join(last_firmware[0], f"{last_firmware[1][-1]['count']}_{last_firmware[0]}_{last_firmware[1][-1]['version']}_{last_firmware[1][-1]['md5']}.bin")
+    print(f'Last firmware path: {last_firmware_path}')
+
     try:
         download_file(CANControl, 'CANControl', CANControl_save_path, CANControl_start, max_count)
         download_file(NewUI, 'NewUI', NewUI_save_path, NewUI_start, max_count)
         download_file(NewTest, 'NewTest', NewTest_save_path, NewTest_start, max_count)
         download_file(Test2, 'Test2', Test2_save_path, Test2_start, max_count)
         download_file(DEVID, 'DEVID', DEVID_save_path, DEVID_start, max_count)
-        download_test0(Test0, Test0_save_path)
+        download_test0(Test0, Test0_save_path, last_firmware_path)
     finally:
         bin_info['CANControl'] = CANControl
         bin_info['NewUI'] = NewUI
